ead4f6de09eea8c148a4607fde0a4cebc39dc88f
Denes-cilwal <dinesh.silwal@wesionary.team>
2021-08-23 17:11:14 +0545

    firebase config added to next app

---
+++ next-fb-auth/README.md
@@ -0,0 +1,5 @@
+## NEXT-JS AUTH WITH FIREBASE
+
+- [] Set up firebase
+- [] Authentication and Context Provider
+- [] Protected Routes
---
+++ next-fb-auth/src/firebaseConfig.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+import * as Process from "process";
+
+const firebaseConfig = {
+    apiKey: Process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+    authDomain: Process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: Process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: Process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: Process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: Process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: Process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+};
+
+// Initialise firebase, if firebase instance doesn't exist
+if(!firebase.apps.length){
+    firebase.initializeApp(firebaseConfig)
+
+}
+
+
